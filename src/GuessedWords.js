c305470058a03fa0e40ba78ed036d0c372f247ef
Utsav Karan <ukaran11@gmail.com>
2021-12-30 18:32:52 +0530

    Utsav | Created the shell comp with test

---
+++ src/GuessedWords.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GuessedWords = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+GuessedWords.propTypes = {
+
+};
+
+export default GuessedWords;
---
+++ src/GuessedWords.test.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr, checkProps } from '../test/testUtils';
+import GuessedWords from './GuessedWords';
+
