c6b41d03bc3c78b0ca242a5f8c6b6d15499096f4
Utsav Karan <ukaran11@gmail.com>
2021-12-31 16:36:14 +0530

    utsav | Prepared Input comp for useState tests

--- src/Input.jsx
+++ src/Input.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import PropTypes from  'prop-types';
 
 const Input = ({ secretWord }) => {
+    const [currentGuess, setCurrentGuess] = React.useState("");
     return (
         <div data-test='component-input'>
-            
+            <form className='form-inline'>
+                <input 
+                    data-test='input-box'
+                    className='mb-2 mx-sm-3'
+                    type='text'
+                    placeholder='Enter Guess'
+                    value={currentGuess}
+                    onChange={(event) => setCurrentGuess(event.target.value)}
+                />
+                <button
+                    data-test='submit-button'
+                    className='btn btn-primary mb-2'
+                >Submit
+                </button>
+            </form>
         </div>
     )
 }
