4188eff8a0af11087f6f197260859c2c0641d73a
Utsav Karan <ukaran11@gmail.com>
2021-12-31 15:10:34 +0530

    Utsav | Added Proptypes check to Input and added test for that

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import PropTypes from  'prop-types';
 
-const Input = (props) => {
+const Input = ({ secretWord }) => {
     return (
         <div data-test='component-input'>
             
         </div>
     )
+}
+
+Input.propTypes = {
+    secretWord: PropTypes.string.isRequired,
 }
 
 export default Input
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { findByTestAttr } from '../test/testUtils';
+import { checkProps, findByTestAttr } from '../test/testUtils';
 import Input from './Input';
 
-const setup = () => {
-    return shallow(<Input />);
+const setup = (secretWord='party') => {
+    return shallow(<Input secretWord={secretWord}/>);
 }
 
 test('Input renders without error', () => {
     const wrapper = setup();
     const inputComponent = findByTestAttr(wrapper, 'component-input');
     expect(inputComponent.length).toBe(1);
+});
+
+test('does not throw warning with expected props', () => {
+    checkProps(Input, { secretWord: 'party'})
 })
