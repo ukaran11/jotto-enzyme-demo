d5b67d89dc15f252269cd23682f1a1b5e2236e8f
Utsav Karan <ukaran11@gmail.com>
2022-01-03 17:46:48 +0530

    Utsav | Completed guessWord action creator and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { getLetterMatchCount } from '../helpers';
+
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
     GUESS_WORD: 'GUESS_WORD',
@@ -7,7 +9,17 @@ export const actionTypes = {
 
 export const guessWord = (guessedWord) => {
     return function(dispatch, getState) {
+        const secretWord = getState().secretWord;
+        const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
 
+        dispatch({
+            type: actionTypes.GUESS_WORD,
+            payload: { guessedWord, letterMatchCount }
+        });
+
+        if(guessedWord === secretWord) {
+            dispatch({ type: actionTypes.CORRECT_GUESS });
+        };
     };
 };
 
--- src/integration.test.js
+++ src/integration.test.js
@@ -61,12 +61,24 @@ describe('guessWord action dispatcher', () => {
                     ...guessedWords,
                     { guessedWord: unsuccessfulGuess, letterMatchCount: 3}
                 ]
-            }
+            };
+
             expect(newState).toEqual(expectedState);
         });
 
         test('updates state correctly for successful guess', () => {
+            store.dispatch(guessWord(secretWord));
+            const newState = store.getState();
+            const expectedState = {
+                secretWord,
+                success: true,
+                guessedWords: [
+                    ...guessedWords,
+                    { guessedWord: secretWord, letterMatchCount: 5}
+                ]
+            };
 
+            expect(newState).toEqual(expectedState);
         });
     })
 });
