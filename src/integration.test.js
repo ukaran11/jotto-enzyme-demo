bea023b09f9e8b31ea53f639cf025c825758893f
Utsav Karan <ukaran11@gmail.com>
2022-01-03 16:21:20 +0530

    Utsav | Added first thunk test

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,10 +2,30 @@ import { storeFactory } from "../test/testUtils";
 import { guessWord } from './actions';
 
 describe('guessWord action dispatcher', () => {
-    
-    describe('no guessed words', () => {
-        test('updates state correctly for unsuccessful guess',() => {
+    const secretWord = 'party';
+    const unsuccessfulGuess = 'train';
 
+    describe('no guessed words', () => {
+        let store;
+        const initialState = { secretWord };
+
+        beforeEach(() => {
+            store = storeFactory(initialState);
+        });
+
+        test('updates state correctly for unsuccessful guess',() => {
+            store.dispatch(guessWord(unsuccessfulGuess));
+            const newState = store.getState();
+            const expectedState = {
+                ...initialState,
+                success: false,
+                guessedWord: [{
+                    guessedWord: unsuccessfulGuess,
+                    letterMatchCount: 3
+                }]
+            };
+
+            expect(newState).toEqual(expectedState);
         });
 
         test('updates state correctly for successful guess', () => {
